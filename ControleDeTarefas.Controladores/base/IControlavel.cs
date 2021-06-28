c2345638b508c0254b071bd86e65454bbcd0d936
Erik Borela <erikborella@gmail.com>
2021-06-28 15:49:34 -0300

    IControlavel movido para Base

--- ControleDeTarefas.Controladores/base/IControlavel.cs
+++ ControleDeTarefas.Controladores/base/IControlavel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ControleDeTarefas.Controladores
+namespace ControleDeTarefas.Controladores.Base
 {
     public interface IControlavel<T>
     {
